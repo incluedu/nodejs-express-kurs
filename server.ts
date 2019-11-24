836cc8761ee487e148eba436391e709e69cd4eaa
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2019-11-24 13:51:24 +0100

    after lesion 5

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/', (req, res, next) => {
+    res.send("Tour Booking API");
+});
+
+app.get('/tours', (req, res, next) => {
+    res.send("Get a list of tours");
+});
+
+app.post('/tours', (req, res, next) => {
+    res.send("Add a tour");
+});
+
+app.listen(process.env.PORT || 8091, () => {
+    console.log('Server started...');
+});
+
